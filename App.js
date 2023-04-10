711ab145b9c93df4eb65fb347add92cf2da1207a
Alice <devalice117@gmail.com>
2023-04-10 11:23:37 -0300

    Finalizando com AsyncStorage

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import Modal from './components/components/modal';
@@ -13,20 +13,82 @@ export default function App() {
   const [modal, setModal] = useState(false)
   const [tarefaAtual, setTarefaAtual] = useState("")
 
-  const [tarefas, setTarefas] = useState( [
-    {
-      nome:"aaaaaaaaaaaaaaaaaaaaa",
-      id:0
-    },
-  
-  ])
+  const [tarefas, setTarefas] = useState( [])
+  var newTarefas = []
 
-  const deleteTarefa = (id) =>{
-    alert("Tarefa com id"+ id +" foi excluida com sucesso")
-    let newTarefas = tarefas.filter(function(val) {
-      return val.id != id
-    })
-    setTarefas(newTarefas)
+  //use effect rodando assim que a aplicação é iniciada
+
+  useEffect(()=>{
+    (async() =>{
+      setModal(false)
+      let id = 0
+      if(tarefas.length > 0){
+        id = tarefas[tarefas.length - 1].id + 1
+      }
+    
+      let tarefa = {id:id,tarefa:tarefaAtual};
+    
+      try {
+        await AsyncStorage.setTarefas([...tarefas, tarefa])
+        if(tarefas == null){
+          setTarefas([])
+        }else{
+          setTarefas(JSON.parse(tarefaAtual)) 
+        }
+      }catch(error){
+        console.error('Erro salvar Tarefa')
+      }
+    
+    }
+  
+    )
+  })
+
+  // Deletar as tarefas que estão salvas localmente
+
+  function deletarTarefa(id){
+    alert('Tarefa com id '+id+' foi deletada com sucesso!');
+    //TODO: Deletar do array/estado a tarefa com id especificado!
+    let newTarefas = tarefas.filter(function(val){
+          return val.id != id;
+    });
+
+    setTarefas(newTarefas);
+   
+    (async () => {
+      try {
+        await AsyncStorage.setItem('tarefas', JSON.stringify(newTarefas));
+        //console.log('chamado');
+      } catch (error) {
+        // Error saving data
+      }
+    })();
+    
+}
+
+
+
+  const salvarTarefi = async() =>{
+    setModal(false)
+    let id = 0
+    if(tarefas.length > 0){
+      id = tarefas[tarefas.length - 1].id + 1
+    }
+  
+    let tarefa = {id:id,tarefa:tarefaAtual};
+  
+    setTarefas([...tarefas, tarefa])
+    console.log(tarefas)
+  
+    (async()=>{
+      try {
+        await AsyncStorage.setItem('tarefas', JSON.stringify(newTarefas))
+        console.log('Chamado')
+      } catch (error) {
+        
+      }
+    })()
+  
   }
 
   const fecharModal = () =>{
@@ -41,20 +103,6 @@ export default function App() {
   }
 }
 
-const salvarTarefi = () =>{
-  setModal(false)
-  let id = 0
-  if(tarefas.length > 0){
-    id = tarefas[tarefas.length - 1].id + 1
-  }
-
-  let tarefa = {id:id,tarefa:tarefaAtual};
-
-  setTarefas([...tarefas, tarefa])
-  console.log(tarefas)
-
-}
-
   return (
     <SafeAreaView style={styles.container}>
       <Modal tarAtual={text => setTarefaAtual(text) } close={fecharModal} visible={modal} salvarTarefa={() => salvarTarefi()}/>
@@ -62,7 +110,7 @@ const salvarTarefi = () =>{
       {
         tarefas.map((val) => {
           return(
-            <ListaDeTarefas key={val.id} nomeDaTarefa={val.tarefa} deleteTarefa={() => {deleteTarefa(val.id)}} />
+            <ListaDeTarefas key={val.id} nomeDaTarefa={val.tarefa} deleteTarefa={() => {deletarTarefa(val.id)}} />
 
           )
         })
